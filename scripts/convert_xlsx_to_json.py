ca54e389d0502369b181ebd7842457f90c7d839c
strolcity <PCF43-Monistrol@proton.me>
2026-07-21 09:25:43 +0200

    json

--- scripts/convert_xlsx_to_json.py
+++ scripts/convert_xlsx_to_json.py
@@ -1,19 +1,13 @@
 import pandas as pd
 import json
 
-# Chemin vers ton fichier XLSX
 input_file = "../data/securite/delinquance.xlsx"
-# Chemin vers le fichier JSON de sortie
 output_file = "../data/securite/delinquance.json"
 
-# Lire le XLSX
 df = pd.read_excel(input_file)
-
-# Convertir en JSON (format "records" = liste de dictionnaires)
 data = df.to_dict(orient="records")
 
-# Sauvegarder en JSON
 with open(output_file, "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 
-print(f"Fichier JSON généré : {output_file}")
+print(f"✅ Fichier JSON généré : {output_file}")
